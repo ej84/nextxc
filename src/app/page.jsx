733623fa58fc8da11f9708bc48dd82e277f0269b
RjCoder <ej84@njit.edu>
2024-03-15 15:27:43 -0400

    sidebar UI updated.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,15 @@
-import { House, Search } from "@mui/icons-material";
+import {
+  BookmarkBorder,
+  Brightness5Outlined,
+  EmailOutlined,
+  ExpandMore,
+  GroupOutlined,
+  House,
+  ListAltOutlined,
+  NotificationsOutlined,
+  PersonOutlineOutlined,
+  Search,
+} from "@mui/icons-material";
 
 export default function Home() {
   return (
@@ -11,6 +22,38 @@ export default function Home() {
         <button className="sidebutton">
           <Search fontSize="large" className="mr-3" />
           <p className="text-2xl focus:font-bold pr-3 mt-0.5">Explore</p>
+        </button>
+        <button className="sidebutton">
+          <NotificationsOutlined fontSize="large" className="mr-3" />
+          <p className="text-2xl focus:font-bold pr-3 mt-0.5">Notifications</p>
+        </button>
+        <button className="sidebutton">
+          <EmailOutlined fontSize="large" className="mr-3" />
+          <p className="text-2xl focus:font-bold pr-3 mt-0.5">Messages</p>
+        </button>
+        <button className="sidebutton">
+          <ListAltOutlined fontSize="large" className="mr-3" />
+          <p className="text-2xl focus:font-bold pr-3 mt-0.5">Lists</p>
+        </button>
+        <button className="sidebutton">
+          <BookmarkBorder fontSize="large" className="mr-3" />
+          <p className="text-2xl focus:font-bold pr-3 mt-0.5">Bookmarks</p>
+        </button>
+        <button className="sidebutton">
+          <GroupOutlined fontSize="large" className="mr-3" />
+          <p className="text-2xl focus:font-bold pr-3 mt-0.5">Communities</p>
+        </button>
+        <button className="sidebutton">
+          <Brightness5Outlined fontSize="large" className="mr-3" />
+          <p className="text-2xl focus:font-bold pr-3 mt-0.5">Premium</p>
+        </button>
+        <button className="sidebutton">
+          <PersonOutlineOutlined fontSize="large" className="mr-3" />
+          <p className="text-2xl focus:font-bold pr-3 mt-0.5">Profile</p>
+        </button>
+        <button className="sidebutton">
+          <ExpandMore fontSize="large" className="mr-3" />
+          <p className="text-2xl focus:font-bold pr-3 mt-0.5">More</p>
         </button>
       </div>
     </main>
